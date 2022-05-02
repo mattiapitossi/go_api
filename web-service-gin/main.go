9d2260dd005dc2ff194d10a8154f0d24e061cefa
MattiaPitossi <mattiapitossi@icloud.com>
2022-05-02 22:13:11 +0200

    Update endpoint for expense tracker app

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,27 +1,52 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type category struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue", Artist: "John", Price: 56.99},
+var categories = []category{
+	{ID: "1", Name: "Bar"},
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getCategories(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, categories)
+}
+
+func postCategory(c *gin.Context) {
+	var newCategory category
+
+	if err := c.BindJSON(&newCategory); err != nil {
+		return
+	}
+
+	categories = append(categories, newCategory)
+	c.IndentedJSON(http.StatusCreated, newCategory)
+}
+
+func getCategoryByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range categories {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "category not found"})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	router.GET("/api/category", getCategories)
+	router.POST("/api/category", postCategory)
+	router.GET("/api/category/:id", getCategoryByID)
+
 	router.Run("localhost:8080")
 }
